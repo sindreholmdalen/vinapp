04a37359f1cacd8f7affb3441c23eb8e27163b25
sindreholmdalen <sindre.dalen@gmail.com>
2026-03-21 10:06:00 +0100

    fix: add tailwind.config.cjs for CJS compat, update postcss.config.js

---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
+
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                wine: {
+                    50: '#fdf2f4',
+                    100: '#fce7eb',
+                    200: '#f9d0d9',
+                    300: '#f4a9b8',
+                    400: '#ed7693',
+                    500: '#e04870',
+                    600: '#cc285a',
+                    700: '#ab1d4a',
+                    800: '#8f1b42',
+                    900: '#7a1b3d',
+                    950: '#44091e',
+                },
+            },
+        },
+    },
+    plugins: [forms],
+};
