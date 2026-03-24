1c40bf0f273f2aecb32d0618845ff74c19f83254
sindreholmdalen <sindre.dalen@gmail.com>
2026-03-24 23:32:50 +0100

    Fix iOS Safari: add safari14 build target

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
+        plugins: [
+                    laravel({
+                                    input: 'resources/js/app.jsx',
+                                    refresh: true,
+                    }),
+                    react(),
+                ],
+        resolve: {
+                    alias: {
+                                    '@': '/resources/js',
+                    },
         },
-    },
+        build: {
+                    target: ['es2020', 'safari14', 'chrome87', 'firefox78'],
+        },
 });
